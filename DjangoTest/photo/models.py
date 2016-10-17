c0b99d91d94db066bbac1641a628f4120720ab35
홍석원 <petrohong@gmail.com>
2016-10-17 15:29:01 +0900

    migrate 방법은 기존 app은 상관이 없지만 추가되는 app은 makemigrate [app]를 먼저 진행하여야 하며, 이후에 syncdb 또는 migrate를 통해 admin에 등록해줘야 db및 화면에 반영 됨.

--- DjangoTest/photo/models.py
+++ DjangoTest/photo/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 # Create your models here.
 class Photo(models.Model):
-    image_file = models.ImageField()
-    filtered_image_file = models.ImageField()
+    id = models.AutoField(primary_key=True)
+    image_file = models.ImageField(upload_to='static_files/uploaded/original/%Y%m%d')
+    filtered_image_file = models.ImageField(upload_to='static_files/uploaded/filtered/%Y%m%d')
     description = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def delete(self, *args, **kwargs):
+        self.image_file.delete()
+        self.filtered_image_file.delete()
+        super(Photo, self).delete(*args, **kwargs)
